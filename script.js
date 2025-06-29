b0d69e38e970e4069b0ea0150f8f04bdb3fb6bd6
Rhafaela22 <5412141@alunouninter.com>
2025-06-29 09:32:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,4 +5,9 @@ const elementoMensagem = document.getElementById('mensagem');
 // Adiciona um evento de clique ao botão.
 botao.addEventListener('click', function() {
   elementoMensagem.textContent = '" Não tenhamos pressa, mas não percamos tempo. "';
+
+  setTimeout(function() {
+    // Após 8 segundos, o texto da mensagem é limpo.
+    elementoMensagem.textContent = '';
+  }, 8000); 
 });
